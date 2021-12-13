5defc2004087d41bf497f4995aa1112984494d68
feiyongjing <2059326740@qq.com>
2021-12-13 21:34:27 +0800

    配置中心敏感数据加密

--- springcloud-goods-portal/src/main/java/com/eric/springcloud/goods/portal/controller/TestController.java
+++ springcloud-goods-portal/src/main/java/com/eric/springcloud/goods/portal/controller/TestController.java
@@ -2,6 +2,7 @@ package com.eric.springcloud.goods.portal.controller;
 
 import com.eric.springcloud.goods.portal.service.GoodsRemoteClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,27 +19,21 @@ public class TestController {
         this.goodsRemoteClient = goodsRemoteClient;
     }
 
+
+    @Value(value = "${aaa.test}")
+    Integer aaaTest;
+
+    @GetMapping("/getTestConfig")
+    public String getTest1() {
+        return "获取的配置信息："+aaaTest;
+    }
+
     @GetMapping("/parent_test")
     public String getTest1(@RequestParam("goodsId") String goodsId) {
-
-//        try {
-//            Thread.sleep(10000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-
-//        String str = null;
-//        if (str == null) {
-//            throw new RuntimeException("消费者异常");
-//        }
         return goodsRemoteClient.getTest(goodsId);
     }
 
 
-//    public String fallback(@RequestParam("goodsId")String goodsId,Throwable throwable){
-//        System.out.println("goodsId是"+goodsId);
-//        return "调用服务时，服务熔断降级了";
-//    }
 
 
 }
