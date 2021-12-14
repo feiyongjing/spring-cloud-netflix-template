d89ba2c445f1c0ab21d1b77d3c261e2611ef16f2
feiyongjing <2059326740@qq.com>
2021-12-14 20:17:00 +0800

    修补配置打开web访问端点错误

--- springcloud-goods-portal/src/main/java/com/eric/springcloud/goods/portal/controller/TestController.java
+++ springcloud-goods-portal/src/main/java/com/eric/springcloud/goods/portal/controller/TestController.java
@@ -3,11 +3,13 @@ package com.eric.springcloud.goods.portal.controller;
 import com.eric.springcloud.goods.portal.service.GoodsRemoteClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/parent_goods")
 public class TestController {
