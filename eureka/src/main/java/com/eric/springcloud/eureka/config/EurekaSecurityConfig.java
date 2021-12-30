7eb192f8f67b2050f51a6879b0d47bed83c231c8
feiyongjing <2059326740@qq.com>
2021-12-30 16:20:08 +0800

    设置Eureka的csrfæ放行路径

--- eureka/src/main/java/com/eric/springcloud/eureka/config/EurekaSecurityConfig.java
+++ eureka/src/main/java/com/eric/springcloud/eureka/config/EurekaSecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class EurekaSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable();
+        http.csrf().ignoringAntMatchers("/eureka/**");
         super.configure(http);
     }
 }
